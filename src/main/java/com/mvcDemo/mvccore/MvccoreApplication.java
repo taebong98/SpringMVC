9ebbc9fc9f5850eaaac722c54fd000091cdab5de
TaeHeyeon_Kim <thk98k@gmail.com>
2022-10-25 16:52:53 +0900

    start Spring project

---
+++ src/main/java/com/mvcDemo/mvccore/MvccoreApplication.java
@@ -0,0 +1,13 @@
+package com.mvcDemo.mvccore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvccoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvccoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvcDemo/mvccore/MvccoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvcDemo.mvccore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvccoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
